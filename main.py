df068d2a2c20611485f4945ce736ad8ae450f05b
Deviur <deviur@yandex.ru>
2022-07-05 12:26:25 +0300

    Add merge functional

--- main.py
+++ main.py
@@ -1,29 +1,64 @@
 import docx
+import os
+
 """
 TODO: write notes here ⤵
 Файл doc покетом docx открыть нельзя.
 """
-doc = docx.Document()
-doc.add_paragraph('Это первый абзац')
-doc.save('example.docx')
-doc = docx.Document('example.docx')
-doc.add_paragraph('Это второй абзац')
-doc.save('example2.docx')
+# doc = docx.Document()
+# doc.add_paragraph('Это первый абзац')
+# doc.save('example.docx')
+# doc = docx.Document('example.docx')
+# doc.add_paragraph('Это второй абзац')
+# doc.save('example2.docx')
+
+
+def get_docx_files():
+    return [file for file in os.listdir() if file.endswith('.docx')]
+
+
+def load_docx_files():
+    with open('context.txt', 'r') as fp:
+        return [line.rstrip() for line in fp.readlines()]
 
 
 def append(from_where: docx.Document, to_where: docx.Document()):
-    for from_sec in from_where.sections:
-        to_sec = to_where.add_section()
-        to_sec = from_sec
+    for el in from_where.element.body:
+        to_where.element.body.append(el)
+    return to_where
+
+
+def main():
+
+    if os.path.isfile('merged.docx'):
+        os.remove('merged.docx')
+
+    try:
+        open('context.txt', 'r')
+    except FileNotFoundError:
+        with open('context.txt', 'w') as fp:
+            files = get_docx_files()
+            for f in get_docx_files():
+                fp.write(f + '\n')
+        return
+
+    docx_files = load_docx_files()
+    merged_document = docx.Document()
+    for file in docx_files:
+        sub_document = docx.Document(file)
+        append(sub_document, merged_document)
+    merged_document.save('merged.docx')
 
 
 if __name__ == '__main__':
-    to_doc = docx.Document()
-
-    from_doc = docx.Document('example.docx')
-    append(from_doc, to_doc)
-
-    from_doc = docx.Document('example2.docx')
-    append(from_doc, to_doc)
-
-    to_doc.save('example3.docx')
+    main()
+    # to_doc = docx.Document()
+    #
+    # from_doc = docx.Document('example.docx')
+    # append(from_doc, to_doc)
+    #
+    # from_doc = docx.Document('example2.docx')
+    # append(from_doc, to_doc)
+    #
+    # to_doc.save('example3.docx')
+    # print(get_docx_files())
